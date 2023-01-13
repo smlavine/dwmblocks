675dc62978b98c8833388058154ee9d95eac1e13
Sebastian LaVine <mail@smlavine.com>
2023-01-13 14:12:47 -0500

    Add signals for intlkey, vol blocks
    CLICKABLE_BLOCKS added weird spacing in the status bar because the
    non-printable control characters were being factored into the length of
    the bar. I don't care about being able to click each individual block of
    the status bar, so I removed it.

--- config.h
+++ config.h
@@ -1,11 +1,10 @@
 #define CMDLENGTH 45
 #define DELIMITER " "
 #define LEADING_DELIMITER
-#define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-    BLOCK("intlkey -i", 5, 0), // TODO: signals
-    BLOCK("vol", 5, 0), // TODO: signals
+    BLOCK("intlkey -i", 5, 17),
+    BLOCK("vol", 5, 18),
     BLOCK("sb-date", 1, 0),
     BLOCK("batinfo", 1, 0),
 };
